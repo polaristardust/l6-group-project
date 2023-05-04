e4e18d66966002912de5d2e2e704f6caf6ad5357
adamben04 <adambenazouz@computingforall.org>
2023-05-04 16:59:31 -0700

    change names

---
+++ internet-safety-app/src/components/Game1.js
@@ -0,0 +1,18 @@
+import './Game1.css';
+
+function Game1() {
+
+    const myStyle = {
+        margin: "0 auto",
+        width: "30vw",
+        border: "0.2vw solid black"
+      }  
+    return (
+
+            <div style={myStyle}>
+                <h1>Game</h1>
+            </div>
+
+    )
+}
+export default Game1;
