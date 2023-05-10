aa1e64feeb8e7d7389ac6ea1d819a4cf3082266d
adamben04 <adambenazouz@computingforall.org>
2023-05-10 05:30:15 -0700

    complete flags for all elements

--- internet-safety-app/src/components/Game.js
+++ internet-safety-app/src/components/Game.js
@@ -3,23 +3,22 @@ import { Link } from "react-router-dom";
 import CautionSign from './images/CautionSign.png'
 import StartNow from './images/StartNow.png'
 import DropdownArrow from './images/DropdownArrow.png'
-// import Flag1Found from './FlagCount.js'
 import React, { useState } from 'react';
-// // import Flag1Found from './FlagCount.js'
-import {Flag1, Flag2, Flag3} from './FlagCount.js'
+// import Flag1Found from './FlagCount.js'
+import {Flag1, Flag2, Flag3, Flag4, Flag5, Flag6, Flag7, Flag8, Flag9, Flag10, Flag11} from './FlagCount.js'
 import Flag1Found from './FlagCount.js'
 
 function Game(props) {
 
-    const [clicked, setClicked] = useState(Array(8).fill(false));
+    // const [clicked, setClicked] = useState(Array(8).fill(false));
     const [modalVisible, setModalVisible] = useState(true);
 
-    function handleClick(index) {
-        const updatedClicked = [...clicked];
-        updatedClicked[index] = true;
-        setClicked(updatedClicked);
+    // function handleClick(index) {
+    //     const updatedClicked = [...clicked];
+    //     updatedClicked[index] = true;
+    //     setClicked(updatedClicked);
 
-    }
+    // }
 
     return (
         <div id="white-background">
@@ -29,7 +28,7 @@ function Game(props) {
                 <button onClick={()=> setModalVisible(false)}>Start</button>
             </div>
             <div>
-                <h2 id="subject" onClick={() => handleClick(0)} className={clicked[0] ? 'green-background' : ''}>{'>>'}!! -You-have won an $90-CVS-Gift-Card</h2>
+                <Flag4 onFlagFound={props.onFlagFound}>{'>>'}!! -You-have won an $90-CVS-Gift-Card</Flag4>
                 <div id="address">
                     <Flag3 onFlagFound={props.onFlagFound}/>
                     <div id="address-text">
@@ -37,7 +36,7 @@ function Game(props) {
                             <h5>-CVS Rewards-
                             <Flag2 onFlagFound={props.onFlagFound}>afjngbapeofkdsfn@cvs.pharmacy.us</Flag2>
                             </h5>
-                            <p id="address-info"><u>via</u> sendgrid.net</p>
+                            <Flag5 onFlagFound={props.onFlagFound}><u>via</u> sendgrid.net</Flag5>
                         </div>
                         <div id="address-to">
                             <p>to me</p>
@@ -53,23 +52,26 @@ function Game(props) {
                     </h2>
                     <div id="red-bar"></div>
                     <h3>
-                        Dear Pharmacy Customer,,<br/><br/>
+                        <Flag6 onFlagFound={props.onFlagFound}>Dear Pharmacy Customer</Flag6>
+                        <Flag7 onFlagFound={props.onFlagFound}>,,</Flag7>
+                        
+                        <br/><br/>
 
-                        <b>Congratulations!</b> You’ve been selected to recieved an <b>EXCLUSIVE OFFER!</b><br/><br/>
+                        <b>Congratulations!</b> You’ve been selected to <Flag10 onFlagFound={props.onFlagFound}>recieved</Flag10> an <b>EXCLUSIVE OFFER!</b><br/><br/>
 
-                        Enter your correct information in this short survey about your experiences with Pharmacy and receive your exclusive reward offer <em><b>(minimum value of $90)</b></em> for participating.
+                        <Flag8 onFlagFound={props.onFlagFound}>Enter your correct information</Flag8> in this short survey about your experiences with Pharmacy and receive your exclusive reward offer <em><b>(minimum value of $90)</b></em> for participating.
                     </h3>
                     <div className="center">
                         <Flag1 onFlagFound={props.onFlagFound}/>
                         <h3 className="centerText">
-                            You have <b>4 minutes and 14 seconds</b> to activate this offer!    
+                            You have <b><Flag9 onFlagFound={props.onFlagFound}>4 minutes and 14 seconds</Flag9></b> to activate this offer!    
                         </h3>
                     </div>
                     <div id="red-bar"></div>
                 </div>
 
                 <p className="center">
-                    This rewards program is not endorsed, sponsored by, or affiliated with any merchants listed above.<br/>
+                    This rewards program is <Flag11 onFlagFound={props.onFlagFound}>not endorsed, sponsored by, or affiliated with any merchants listed above.</Flag11><br/>
                     Registered trademarks are the property of their own respective owners.
                 </p>
             </div>
