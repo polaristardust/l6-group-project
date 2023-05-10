a6a33fe659b83aaaa0a213e65936c119a783e870
Tao Hoang <taohoang01@gmail.com>
2023-05-09 18:18:46 -0700

    fixed info connecting to app.js

--- internet-safety-app/src/App.js
+++ internet-safety-app/src/App.js
@@ -1,11 +1,11 @@
 import './App.css';
 import NavBar from './components/NavBar.js';
 import BackgroundImage from './components/images/BackgroundImage.jpg'
-import Game from './components/Game.js'
-// import Info from './components/Info.js';
+import Game from './components/Game.js';
+import Info from './components/Info.js';
 import React, { useState } from "react";
 import ReactModal from 'react-modal';
-import Flag1Found from './components/FlagCount.js'
+import Flag1Found from './components/FlagCount.js';
 
 function App() {
 
