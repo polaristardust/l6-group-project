92f564ed49f299be564efce39bcb18cda947d1d6
adamben04 <adambenazouz@computingforall.org>
2023-05-10 19:51:37 -0700

    change site name

--- internet-safety-app/src/components/NavBar.js
+++ internet-safety-app/src/components/NavBar.js
@@ -5,7 +5,7 @@ function NavBar() {
     return (
         <div className="nav-bar">
             <div className="nav-logo">
-                <h1>website name</h1>
+                <h1>Scammio</h1>
             </div>
             <div className='pages'>
                 <Link to="/">Information</Link>
